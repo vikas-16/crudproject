ad2bda7006ece50b9cc016332014e4eb16eb144b
itexperts083 <pulkitmadaan.ameotech@gmail.com>
2022-05-23 12:04:05 +0530

    opeartion changes delete

--- enroll/views.py
+++ enroll/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from .form import Studentregistration
 # from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import make_password
 from .form import *
 # Create your views here.
+
+########## This Function show add item###############
 
 def add_show(request):
     fm = Studentregistration()
@@ -11,8 +14,7 @@ def add_show(request):
     if request.method == 'POST':
         fm = Studentregistration(request.POST)
         if fm.is_valid():
-        #  cm =fm.cleaned_data['name']
-        #  gm =fm.cleaned_data['email']
+       
             tm =fm.cleaned_data['password']
             print(tm,'===========tm')
         #  rgm = User(name=cm, email=gm, password=tm)
@@ -24,7 +26,12 @@ def add_show(request):
             fm = Studentregistration()
         else:
            pass
-     
-        return render(request, 'enroll/addandshow.html', {'form':fm,'stu':stud})
-    return render(request, 'enroll/addandshow.html', {'form':fm,'stu':stud})    
+    return render(request, 'enroll/addandshow.html', {'form':fm,'stu':stud})   
+
+#########THIS DELETE FUNCTION###########
+def delete_data(request, id):
+    if request.method == 'POST':
+     pi = User.objects.get(pk=id)
+     pi.delete()
+     return HttpResponseRedirect('/')
 
